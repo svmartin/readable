1b8ca074caa75e46a3036b95900d7449341d5e53
SM <sam@Apples-MacBook-Pro.local>
2017-11-27 17:29:21 -0500

    add react router

--- readable-front/src/App.js
+++ readable-front/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import Menu from './components/Menu';
 import Categories from './components/Categories'
 
 
 class App extends Component {
+
+  // state = {
+  //   posts: null
+  // }
+  //
+  // componentDidMount() {
+  //   fetch('url')
+  //     .then(res => res.json())
+  //     .then(posts => {
+  //       this.setState({ posts })
+  //     })
+  // }
   render() {
     return (
       <div className="container">
--- readable-front/src/components/Categories.js
+++ readable-front/src/components/Categories.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
 import Menu from './Menu';
 import Category from './Category'
 
@@ -6,9 +7,13 @@ const Categories = () => {
   return (
     <div>
       <div className="columns">
-        <Category customClass={"notification is-info title"} title={"React"}/>
-        <Category customClass={"notification is-success title"} title={"Redux"}/>
-        <Category customClass={"notification is-warning title"} title={"Udacity"}/>
+        <Link to="/react"><Category
+          customClass={"notification is-info title"}
+          title={"React"}/></Link>
+        <Link to="redux"><Category
+          customClass={"notification is-success title"}
+          title={"Redux"}/></Link>
+        <Link to="udacity"><Category customClass={"notification is-warning title"} title={"Udacity"}/></Link>
       </div>
     </div>
   );
